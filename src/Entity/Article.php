0fa656ba11f8b506aec1697c6bde7f10e0365db4
Martin Hoelzer <mpunkth@martsite.de>
2020-09-18 15:21:11 +0200

    added description and category to Article Entity, added new migration

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -37,6 +37,16 @@ class Article
      */
     private $weight;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +96,30 @@ class Article
     public function setWeight(?int $weight): self
     {
         $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
