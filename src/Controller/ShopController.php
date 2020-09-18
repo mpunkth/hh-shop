975ada3959b3d37887f5bace2a3fac988680cdda
Martin Hoelzer <mpunkth@martsite.de>
2020-09-18 13:02:09 +0200

    added controller and form type classes

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,22 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShopController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
